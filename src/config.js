c6177942ff9a6d99b5343f5796acd0498036c9f6
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-10-26 10:20:05 -0300

    chore: remove blank space from file names

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ export default {
       'The child had looked so excited when the clown had presented a large red balloon. You had seen this, but in the throes of your morning commute you didn’t register it until it was too late. Who asked the government to support a fair right through Main Street during a week day anyway? Your bike barrelled right between the child and the clown and sent the balloon on its merry way. You didn’t turn back to see the damage you had done. Later you saw the balloon floating outside your office window.',
     colorBg: '#D5E9F6',
     colorText: '#4E43FA',
-    illustration: require('./images/Balloon Lost.png'),
+    illustration: require('./images/BalloonLost.png'),
   },
   'Boat Leak': {
     title: 'There’s a Leak in the Website',
@@ -21,7 +21,7 @@ export default {
       'The boat had looked good to the naked eye. But you wear a very strong prescription and should have been wearing glasses. As you cling on to the bouey the coast guard had thrown at you, you watch the water rush into you beloved dingy. The leak sprays water higher and higher. Then the boat was swallowed and sunk into the abyss.',
     colorBg: '#EFEDFA',
     colorText: '#684CFF',
-    illustration: require('./images/Boat Leak.png'),
+    illustration: require('./images/BoatLeak.png'),
   },
   'Broken Clock': {
     title: 'This Page is Broken',
@@ -29,7 +29,7 @@ export default {
       'A broken clock is right twice a day. But if you just have one clock, it’s impossible to tell exactly when the clock is right. So it could be right at any moment. And that brings you to the crux of the conceptualization. What is time? Nothing but an abyss. Clocks are just false attempts to harness its power. It’s cruel really.',
     colorBg: '#FFE4EA',
     colorText: '#709BFA',
-    illustration: require('./images/Broken Clock.png'),
+    illustration: require('./images/BrokenClock.png'),
   },
   'Broken Mug': {
     title: 'This Page is Broken',
@@ -37,7 +37,7 @@ export default {
       'This mug was a family heirloom. Of your neighbor. Your neighbor always loved the color, shape, and quantity of coffee held by this mug. But your neighbor moved out and left it on their porch, no explanation, no repair materials, no nothing. So you have this broken mug.',
     colorBg: '#D5E9F6',
     colorText: '#2F8E89',
-    illustration: require('./images/Broken Mug.png'),
+    illustration: require('./images/BrokenMug.png'),
   },
   'Burnt Toast': {
     title: 'This Page is Burnt to a Crisp',
@@ -45,7 +45,7 @@ export default {
       'Toast takes so long to make. You stare at the toaster tapping your feet. Your laundry is in the dryer and it just dinged. Maybe you’ll take it out. After all, you have time. You take out your laundry. You fold your underwear. You think about folding your socks. You remember your toast! It is too late. It’s burnt to a crisp. The process repeats itself. You should probably figure out your toasting settings.',
     colorBg: '#FFF4BB',
     colorText: '#4376C0',
-    illustration: require('./images/Burnt Toast.png'),
+    illustration: require('./images/BurntToast.png'),
   },
   'Caution Tape': {
     title: 'Caution! This Page is Cordoned Off',
@@ -53,7 +53,7 @@ export default {
       'The earthquake was not good to the bike lane on your way to work. A large gap in the pavement (too big to be called a pothole) had swallowed three oblivious bikers whole. So the city had put up two pylons and yellow caution tape. Pretty frustrating for you given your propensity to do 360 jumps over the gap.',
     colorBg: '#83D9C8',
     colorText: '#965418',
-    illustration: require('./images/Caution Tape.png'),
+    illustration: require('./images/CautionTape.png'),
   },
   'Dog Ate': {
     title: 'A Dog Ate this Page',
@@ -61,7 +61,7 @@ export default {
       'Your dog is cute but honestly a menace. Where are my shoes? Where is my graduation certificate? Where is the chocolate cake I baked for my Aunt’s birthday? And why did you take your dog to the vet on that same Thursday?!',
     colorBg: '#FFE4EA',
     colorText: '#824B2F',
-    illustration: require('./images/Dog Ate.png'),
+    illustration: require('./images/DogAte.png'),
   },
   Ghost: {
     title: 'This Page is a Ghost',
@@ -77,7 +77,7 @@ export default {
       'People questioned your desire to get strawberry. “That’s the worst flavor,” they said. But you are strong and independent so you got it anyway. And honestly, it wasn’t great. Luckily, two licks in a bike whizzed past you and knocked the cone out of your hand. “Oh no!” you yelled as the creamy pink became a mess in the dirt. But really you were happy.',
     colorBg: '#FFF4BB',
     colorText: '#E896ED',
-    illustration: require('./images/Ice Cream Spill.png'),
+    illustration: require('./images/IceCreamSpill.png'),
   },
   'Kids Toy': {
     title: 'This Page is Wrong',
@@ -85,7 +85,7 @@ export default {
       'You have been trying for ten minutes. It’s pretty late at night and pretty dark in your room. You reach over and flick on a lamp. You feel oh so stupid. The gap in the toy is a triangle and you only have the cylinder and cube pieces. In dismay you toss the toy aside. Curse your five year old’s inability to keep track of the triangle!',
     colorBg: '#D5E9F6',
     colorText: '#DF245D',
-    illustration: require('./images/Kid_s Toy.png'),
+    illustration: require('./images/Kid_sToy.png'),
   },
   'Loch Ness': {
     title: 'This Page is Not Real',
@@ -93,7 +93,7 @@ export default {
       'The imposing figure with the trenchcoat shows you the two polaroids. One appears to show the Loch Ness monster herself in the middle of a stretch of dark water. The other shows a sasquatch picking it’s way through a dark forest. You look closer. The animal shapes are drawn on with ink. “This isn’t real!” You scream and throw the polaroids to the floor, sobbing.',
     colorBg: '#EFEDFA',
     colorText: '#092048',
-    illustration: require('./images/Loch Ness.png'),
+    illustration: require('./images/LochNess.png'),
   },
   'Lost Keys': {
     title: 'This Page is Lost',
@@ -101,7 +101,7 @@ export default {
       'You bought a little bracelet for the express purpose of not losing your keys. You put a hook on your door specifically meant for keeping your keys. You briefly attempted to attach your keys to your phone. But here they are. In the dirt. In the park across the street from that bar you used to like but decided the last time you went that you probably wouldn’t go again. You’ll never find them.',
     colorBg: '#FFF4BB',
     colorText: '#226FAB',
-    illustration: require('./images/Lost Keys.png'),
+    illustration: require('./images/LostKeys.png'),
   },
   'Lost Tourist': {
     title: 'This Page is Not on the Map',
@@ -109,7 +109,7 @@ export default {
       'You told your friends you weren’t bringing your phone, to try and experience what travel was like back in the day. You bought a map and a bottle of water and carried your camera for the money shot. But the map was from 2005 and the landscape had changed. So here you are, in the middle of a large field, that the map continues to claim is a local grocer.',
     colorBg: '#83D9C8',
     colorText: '#386177',
-    illustration: require('./images/Lost Keys.png'),
+    illustration: require('./images/LostKeys.png'),
   },
   Ostrich: {
     title: 'This Page is Buried in the Sand',
@@ -125,7 +125,7 @@ export default {
       'A perfectly enticing pizza box sitting on a table. You open it filled with anticipation. And find… nothing but scraps. Perhaps a half eaten crust. And a lot of grease. The anticipation turns to deep disappointment and despair. There’s nothing left!',
     colorBg: '#83D9C8',
     colorText: '#386177',
-    illustration: require('./images/Pizza Box.png'),
+    illustration: require('./images/PizzaBox.png'),
   },
   'Shoes Tied': {
     title: 'Oh no! We tripped up!',
@@ -133,7 +133,7 @@ export default {
       'You had been told you should always check your shoes before getting up from the bleachers. Freshman were known to walk under them and tie peoples’ shoes together.',
     colorBg: '#FFF4BB',
     colorText: '#180AD8',
-    illustration: require('./images/Shoes Tied.png'),
+    illustration: require('./images/ShoesTied.png'),
   },
   'Spilled Milk': {
     title: 'Don’t Cry Over Spilled Page',
@@ -141,7 +141,7 @@ export default {
       'Gulp. You hold back tears as the white liquid spreads across the floor from your sad looking carton. You should have bought the chocolate milk, it was clearly the better choice. And then maybe you wouldn’t have so carelessly smacked it across the room when you emphatically pointed at a bird outside. Too late now. You wipe the single tear from your eye and go fetch the mop.',
     colorBg: '#FFE4EA',
     colorText: '#673164',
-    illustration: require('./images/Spilled Milk.png'),
+    illustration: require('./images/SpilledMilk.png'),
   },
   Trash: {
     title: 'This Page is in the Garbage',
@@ -157,7 +157,31 @@ export default {
       'The page you were looking for appears to have been moved, deleted, or does not exist.',
     colorBg: '#FFFFFF',
     colorText: '#2F495E',
-    illustration: require('./images/Confused Travolta.png'),
+    illustration: require('./images/ConfusedTravolta.png'),
     maxWidth: '45rem',
+  },
+  Space: {
+    title: "Ooops. The page you're looking for got lost in space.",
+    description: 'The address might be mistyped or the page may have moved.',
+    colorBg: '#DBECF9',
+    colorText: '#2C3E50',
+    illustration: require('./images/Space.png'),
+  },
+  Mailchimp: {
+    title: 'We lost this page',
+    description:
+      "We searched high and low but couldn't find what you're looking for. Let's find a better place for you to go.",
+    colorBg: '#FFFFFF',
+    colorText: '#403B3B',
+    illustration: require('./images/Mailchimp.png'),
+    maxWidth: '45rem',
+  },
+  UnDraw: {
+    title: 'Oops!',
+    description: "That page wasn't found. Don't worry, we're looking into it!",
+    colorBg: '#e7e5fe',
+    colorText: '#2f2b6e',
+    illustration: require('./images/UnDraw.png'),
+    maxWidth: '35rem',
   },
 }
